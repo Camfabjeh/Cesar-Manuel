23335c497b9eb6d501b3c44fb8996f7854447c2e
Camfabjeh <camille.fabry@yahoo.com>
2023-07-04 23:14:21 +0200

    retrait import d'un composant non existant dans App.jsx

--- backend/database.sql
+++ backend/database.sql
@@ -1,6 +1,30 @@
-CREATE TABLE item (
-  id int(11) UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  title varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+DROP TABLE IF EXISTS artist;
+DROP TABLE IF EXISTS photo_report;
+DROP TABLE IF EXISTS photo;
 
-INSERT INTO item (title) VALUES ('Stuff'), ('Doodads');
+
+CREATE TABLE artist (
+  id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  artist_name varchar(255) NOT NULL,
+  website varchar(255)
+);
+
+CREATE TABLE photo_report (
+  id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  report_name varchar(255) NOT NULL,
+  report_date date NOT NULL,
+  report_description varchar(255) NOT NULL,
+  place varchar(255) NOT NULL,
+  artist_id integer, 
+  isHomePage BOOLEAN,
+  CONSTRAINT artist_id_fk FOREIGN KEY (artist_id) REFERENCES artist(id)
+);
+
+CREATE TABLE photo (
+  id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  photo_report_id INTEGER,
+  src varchar(255) NOT NULL,
+  alt varchar(255) NOT NULL,
+  isOnAboutPage BOOLEAN,
+  CONSTRAINT photo_report_id_fk FOREIGN KEY (photo_report_id) REFERENCES photo_report(id)
+);
