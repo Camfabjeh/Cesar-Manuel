399dfbce9aa9637c41f02b0e2b5a3e71890f9a55
Camfabjeh <camille.fabry@yahoo.com>
2023-07-06 12:00:17 +0200

    mise en place des routes en front

--- frontend/src/layout/AdminLayout.jsx
+++ frontend/src/layout/AdminLayout.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable import/no-extraneous-dependencies */
 import React from "react";
 import { Outlet } from "react-router-dom";
 import NavBarAdmin from "../components/NavBarAdmin";
--- frontend/src/layout/UserLayout.jsx
+++ frontend/src/layout/UserLayout.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-// eslint-disable-next-line import/no-extraneous-dependencies
 import { Outlet } from "react-router-dom";
 import NavBarUser from "../components/NavBarUser";
 
