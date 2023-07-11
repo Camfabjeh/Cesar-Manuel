33e35c6805d70d30a7204113cc68579b95d4fae0
Camfabjeh <camille.fabry@yahoo.com>
2023-07-11 20:06:49 +0200

    modification de la casse de photoreportControllers dans router pour ajustement avec main déjà corrigée

--- frontend/src/components/AdminNavBar.jsx
+++ frontend/src/components/AdminNavBar.jsx
@@ -1,7 +1,47 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import BACKOFFICELOGO from "../assets/backoffice/BACK_OFFICE_LOGO.png";
+import BACKOFFICEDECO from "../assets/backoffice/BACK_OFFICE_DECONNECT.png";
 
 function NavBarAdmin() {
-  return <div>NavBarAdmin</div>;
+  return (
+    <div className="h-screen">
+      <div className="flex justify-between bg-black w-60 h-full text-lightgrey pl-5 flex-col items-left">
+        <div className="pb-8">
+          <Link to="/">
+            <img
+              src={BACKOFFICELOGO}
+              alt="trio de couleurs permettant de revenir à la page d'accueil du site"
+            />
+          </Link>
+        </div>
+        <div className="pb-12 flex">
+          <Link className="pt-2 font-title" to="/admin/reportages">
+            Reportages
+          </Link>
+        </div>
+        <div className="pb-12 flex">
+          <Link className="pt-2 font-title" to="/admin/artistes">
+            Artistes
+          </Link>
+        </div>
+        <div className="pb-12 flex">
+          <Link className="pt-2 font-title" to="/admin/photos">
+            Photos
+          </Link>
+        </div>
+        <Link to="/" className="font-title">
+          <img
+            src={BACKOFFICEDECO}
+            alt="bouton de déconnexion administratrice"
+          />
+          Déconnexion
+        </Link>
+      </div>
+    </div>
+  );
 }
+
+// link du backofficedeco à revoir
 
 export default NavBarAdmin;
