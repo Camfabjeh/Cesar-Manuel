ea7ef3db4f2edd372561ed60099b3fc16a54a07a
Camfabjeh <camille.fabry@yahoo.com>
2023-07-19 12:00:43 +0200

    finalisation style navbar user

--- frontend/src/components/UserNavBar.jsx
+++ frontend/src/components/UserNavBar.jsx
@@ -1,38 +1,37 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function NavBarUser() {
   return (
     <nav className="flex-wrap relative flex w-full items-center justify-between bg-lightgrey">
       <div className="!visible hidden flex-grow basis-[100%] items-center lg:!flex lg:basis-auto">
         <ul className="list-style-none flex flex-col pl-0 lg:flex-row ml-auto p-2 font-text font-semibold">
-          <li className="lg:mb-0 lg:pr-2">
-            <Link to="/">Accueil</Link>
-          </li>
-          <li className="mb-4 lg:mb-0 lg:pr-2">
-            <Link
-              className={({ isActive }) =>
-                isActive
-                  ? "text underline underline-offset-8 decoration-2 decoration-yellow"
-                  : ""
-              }
-              to="/listereportages"
-            >
-              Reportages
-            </Link>
-          </li>
-          <li className="lg:mb-0 lg:pr-2">
-            <Link
-              className={({ isActive }) =>
-                isActive
-                  ? "text underline underline-offset-8 decoration-2 decoration-yellow"
-                  : ""
-              }
-              to="/apropos"
-            >
-              À propos
-            </Link>
-          </li>
+          <div className="flex gap-5">
+            <li className="mb-4 lg:mb-0 lg:pr-2">
+              <NavLink
+                className={({ isActive }) =>
+                  isActive
+                    ? "text underline underline-offset-8 decoration-2 decoration-yellow"
+                    : ""
+                }
+                to="/listereportages"
+              >
+                Reportages
+              </NavLink>
+            </li>
+            <li className="lg:mb-0 lg:pr-2">
+              <NavLink
+                className={({ isActive }) =>
+                  isActive
+                    ? "text underline underline-offset-8 decoration-2 decoration-yellow"
+                    : ""
+                }
+                to="/apropos"
+              >
+                À propos
+              </NavLink>
+            </li>
+          </div>
         </ul>
       </div>
     </nav>
