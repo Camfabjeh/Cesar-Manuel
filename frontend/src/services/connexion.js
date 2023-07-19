e8e65eb6b299c4fc1cd9a3231dc643c6e93950fa
Camfabjeh <camille.fabry@yahoo.com>
2023-07-19 16:29:37 +0200

    hotfix du post sur photoreportsadmin

--- frontend/src/services/connexion.js
+++ frontend/src/services/connexion.js
@@ -28,6 +28,26 @@ const updateData = (url, body) => {
   }).catch((err) => console.error(err));
 };
 
+const postFile = (url, form) => {
+  return fetch(`${import.meta.env.VITE_BACKEND_URL}${url}`, {
+    body: form,
+    method: "POST",
+    credentials: "include",
+  })
+    .then((res) => res.json())
+    .catch((err) => console.error(err));
+};
+
+const putFile = (url, form) => {
+  return fetch(`${import.meta.env.VITE_BACKEND_URL}${url}`, {
+    body: form,
+    method: "PUT",
+    credentials: "include",
+  })
+    .then((res) => res)
+    .catch((err) => console.error(err));
+};
+
 const deleteData = (path) => {
   return fetch(`${import.meta.env.VITE_BACKEND_URL}${path}`, {
     method: "DELETE",
@@ -41,6 +61,8 @@ const connexion = {
   post: (path, data) => postData(path, data),
   put: (url, body) => updateData(url, body),
   delete: (path) => deleteData(path),
+  postFile: (url, form) => postFile(url, form),
+  putFile: (url, form) => putFile(url, form),
 };
 
 export default connexion;
