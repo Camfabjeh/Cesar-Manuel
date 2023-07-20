f9ad7b022bb0722ae0ff12d7a9cb712de9c0c99b
Camfabjeh <camille.fabry@yahoo.com>
2023-07-20 11:53:20 +0200

    import du header sur pages front

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import BANNIERE_PAGES from "../assets/front/BANNIERE_PAGES.png";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="bg-cover bg-no-repeat bg-center bg-lightgrey">
+      <img
+        src={BANNIERE_PAGES}
+        alt="logo et nom de l'artiste + texte Ces mains façonnent, ça me fascine"
+      />
+    </div>
+  );
 }
 
 export default Header;
