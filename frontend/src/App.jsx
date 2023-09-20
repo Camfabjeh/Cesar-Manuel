f91881718ebdf70bb656ade8af170abcb6ca1513
Camfabjeh <camille.fabry@yahoo.com>
2023-09-20 12:13:08 +0200

    modification de l'emplacement de la route auth

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -21,13 +21,14 @@ function App() {
       <Routes>
         <Route path="" element={<HomePage />} />
         <Route path="*" element={<Nowhere />} />
+
         <Route path="/" element={<UserLayout />}>
           <Route path="/reportages" element={<PhotoReportsList />} />
           <Route path="/reportages/:id" element={<PhotoReport />} />
-          <Route path="" element={<Auth />} />
           <Route path="/apropos" element={<About />} />
         </Route>
         <Route path="/admin" element={<AdminLayout />}>
+          <Route path="/admin/auth" element={<Auth />} />
           <Route path="/admin/reportages" element={<PhotoReportsAdmin />} />
           <Route path="/admin/" element={<ArtistsAdmin />} />
           <Route path="/admin/photos" element={<PhotosAdmin />} />
